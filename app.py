c9074c50fda60786edda8d2eabeeb899aad618e3
medelmat <mohamed.elmatror@gmail.com>
2022-07-08 14:44:50 +0200

    change port from 8080 to 8000

--- app.py
+++ app.py
@@ -102,4 +102,4 @@ def delete_photo(name):
 	return redirect('/search-photos')   
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=8080, debug=True)
+	app.run(host='0.0.0.0', port=8000, debug=True)
